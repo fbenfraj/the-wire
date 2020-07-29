97e9268dac5d04b41c4a88eb2dae79bb19a5523a
Léopold Szabatura <l.szabatura@gmail.com>
2020-07-29 19:51:19 +0200

    Add users popupMenu

--- the-wire-app/screens/users.js
+++ the-wire-app/screens/users.js
@@ -1,22 +1,72 @@
 import React, { Component } from "react";
-import { Container, Content, Text } from "native-base";
+import { Container, Content, Text, ListItem, Body, Right, Icon, Button } from "native-base";
+import Menu, { MenuItem } from 'react-native-material-menu';
 
-const UsersScreen = () => {
-  return (
-    <Container>
-      <Content>
-        <Text
-          style={{
-            textAlign: "center",
-            flex: 1,
-            textAlignVertical: "center",
-          }}
-        >
-          Users Screen
-        </Text>
-      </Content>
-    </Container>
-  );
+const users = [{
+  name: 'User 1'
+}, {
+  name: 'User 2'
+}, {
+  name: 'User 3'
+}]
+
+class UsersScreen extends React.PureComponent {
+
+  _menu = null
+ 
+  setMenuRef = ref => {
+    this._menu = ref
+  }
+ 
+  hideMenu = () => {
+    this._menu.hide()
+  }
+ 
+  showMenu = () => {
+    this._menu.show()
+  }
+
+  render () {
+    return (
+      <Container>
+        <Content>
+          {users.map((user, i) => (
+            <ListItem
+              key={i}
+              noIndent
+              noBorder
+            >
+              {/* <Left>
+                <Thumbnail
+                  source={{
+                    uri:
+                      "https://vignette.wikia.nocookie.net/avatar-the-last-airbender-discord/images/2/23/PFP2.jpg/revision/latest?cb=20180911165028",
+                  }}
+                ></Thumbnail>
+              </Left> */}
+              <Body>
+                <Text>{user.name}</Text>
+                {/* <Text note>How are you Younes?</Text> */}
+              </Body>
+              <Right>
+                <Menu
+                  ref={this.setMenuRef}
+                  button={
+                    <Button onPress={this.showMenu} transparent>
+                      <Icon name="more-vert" type="MaterialIcons" />
+                    </Button>
+                  }
+                >
+                  <MenuItem onPress={this.hideMenu}>Menu item 1</MenuItem>
+                  <MenuItem onPress={this.hideMenu}>Menu item 2</MenuItem>
+                </Menu>
+              </Right>
+            </ListItem>
+          ))}
+        </Content>
+      </Container>
+    );
+  }
 };
 
 export default UsersScreen;
